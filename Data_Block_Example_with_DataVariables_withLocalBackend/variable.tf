2f8c65e76bd6083d923523cc3a159b5941fd0ca3
srd7395 <121183817+srd7395@users.noreply.github.com>
2023-02-03 17:09:20 -0500

    Data_Block_Example_with_DataVariables_withLocalBackend

--- Data_Block_Example_with_DataVariables_withLocalBackend/variable.tf
+++ Data_Block_Example_with_DataVariables_withLocalBackend/variable.tf
@@ -2,18 +2,22 @@ variable "vnet" {
   type = string
   default = "vnet1"
 }
+
 variable "vnetaddress" {
   type = list
   default = ["17.0.0.0/16"]
 }
+
 variable "subnet" {
   type = string
   default = "subnet1"
 }
+
 variable "subnetaddress" {
   type = list
   default = ["17.0.0.0/24"]
 }
+
 variable "path" {
   default = "C:/Terraform_code/DataRG/terraform.tfstate"
 }
