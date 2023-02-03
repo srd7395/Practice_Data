8c58956f1cb1cbd5b64534519c35c2fc3812f3e3
srd7395 <121183817+srd7395@users.noreply.github.com>
2023-02-03 17:10:04 -0500

    for_each_using_List_Toset

--- foreach_using_List_Toset/main.tf
+++ foreach_using_List_Toset/main.tf
@@ -10,5 +10,5 @@ resource "azurerm_managed_disk" "example" {
   resource_group_name  = azurerm_resource_group.example.name
   storage_account_type = "Standard_LRS"
   create_option        = "Empty"
-  disk_size_gb         = "1"
+  disk_size_gb         = "4"
 }
